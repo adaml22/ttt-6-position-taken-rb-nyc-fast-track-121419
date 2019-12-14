d9e3a90f880801a527c569a955f32845ba3ff709
Adam Longman <boardhead89@gmail.com>
2019-12-14 19:43:55 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,12 @@
 # code your #position_taken? method here!
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def position_taken(board[1])
+  if board[1] = "O"
+    true
+  elsif board[1] = "X"
+    true
+  else
+    false
+end
